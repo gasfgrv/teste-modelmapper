cb768f8ee9b6bdbcb01a95fbef24fca7717b8eba
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-11-10 12:26:41 -0300

    refatoração, docker e collection

--- src/main/java/br/com/example/testemodelmapper/config/ModelMapperConfig.java
+++ src/main/java/br/com/example/testemodelmapper/config/ModelMapperConfig.java
@@ -1,15 +1,43 @@
 package br.com.example.testemodelmapper.config;
 
+import br.com.example.testemodelmapper.dto.ContaDTO;
+import br.com.example.testemodelmapper.model.Conta;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.DestinationSetter;
+import org.modelmapper.spi.SourceGetter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfig {
 
+    private static final SourceGetter<Conta> GET_AGENCIA_CONTA_PAGAMENTO = Conta::getAgenciaContaPagamento;
+    private static final SourceGetter<Conta> GET_NUMERO_CONTA_CONTA_PAGAMENTO = Conta::getNumeroContaContaPagamento;
+    private static final SourceGetter<Conta> GET_DIGITO_CONTA_PAGAMENTO = Conta::getDigitoContaPagamento;
+
+    public static final SourceGetter<Conta> GET_AGENCIA_CONTA_CORRENTE = Conta::getAgencia;
+    public static final SourceGetter<Conta> GET_NUMERO_CONTA_CONTA_CORRENTE = Conta::getNumeroConta;
+    public static final SourceGetter<Conta> GET_DIGITO_CONTA_CORRENTE = Conta::getDigito;
+
+    private static final DestinationSetter<ContaDTO, String> SET_AGENCIA = ContaDTO::setAgencia;
+    private static final DestinationSetter<ContaDTO, String> SET_NUMERO_CONTA = ContaDTO::setNumeroConta;
+    private static final DestinationSetter<ContaDTO, String> SET_DIGITO = ContaDTO::setDigito;
+
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Conta.class, ContaDTO.class, "typeMapContaPagamento")
+                .addMapping(GET_AGENCIA_CONTA_PAGAMENTO, SET_AGENCIA)
+                .addMapping(GET_NUMERO_CONTA_CONTA_PAGAMENTO, SET_NUMERO_CONTA)
+                .addMapping(GET_DIGITO_CONTA_PAGAMENTO, SET_DIGITO);
+
+        modelMapper.createTypeMap(Conta.class, ContaDTO.class, "typeMapContaCorrente")
+                .addMapping(GET_AGENCIA_CONTA_CORRENTE, SET_AGENCIA)
+                .addMapping(GET_NUMERO_CONTA_CONTA_CORRENTE, SET_NUMERO_CONTA)
+                .addMapping(GET_DIGITO_CONTA_CORRENTE, SET_DIGITO);
+
+        return modelMapper;
     }
 
 }
--- src/main/java/br/com/example/testemodelmapper/mapper/ContaConverter.java
+++ src/main/java/br/com/example/testemodelmapper/mapper/ContaConverter.java
@@ -12,33 +12,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class ContaConverter {
 
-    public static final SourceGetter<Conta> GET_AGENCIA_CONTA_PAGAMENTO = Conta::getAgenciaContaPagamento;
-    public static final DestinationSetter<ContaDTO, String> SET_AGENCIA = ContaDTO::setAgencia;
-
-    public static final SourceGetter<Conta> GET_NUMERO_CONTA_CONTA_PAGAMENTO = Conta::getNumeroContaContaPagamento;
-    public static final DestinationSetter<ContaDTO, String> SET_NUMERO_CONTA = ContaDTO::setNumeroConta;
-
-    public static final SourceGetter<Conta> GET_DIGITO_CONTA_PAGAMENTO = Conta::getDigitoContaPagamento;
-    public static final DestinationSetter<ContaDTO, String> SET_DIGITO = ContaDTO::setDigito;
-
     private final ModelMapper mapper;
 
     @Autowired
     public ContaConverter(ModelMapper mapper) {
         this.mapper = mapper;
-        criarTypeMapParaContaPagamento();
-    }
-
-    private void criarTypeMapParaContaPagamento() {
-        this.mapper.createTypeMap(Conta.class, ContaDTO.class, "typeMapContaPagamento")
-                .addMapping(GET_AGENCIA_CONTA_PAGAMENTO, SET_AGENCIA)
-                .addMapping(GET_NUMERO_CONTA_CONTA_PAGAMENTO, SET_NUMERO_CONTA)
-                .addMapping(GET_DIGITO_CONTA_PAGAMENTO, SET_DIGITO);
     }
 
     public ContaDTO convert(Conta conta) {
-        return conta.getTipoConta().equals(TipoContaEnum.CONTA_PAGAMENTO)
+        return TipoContaEnum.CONTA_PAGAMENTO.equals(conta.getTipoConta())
                 ? mapper.map(conta, ContaDTO.class, "typeMapContaPagamento")
-                : mapper.map(conta, ContaDTO.class);
+                : mapper.map(conta, ContaDTO.class, "typeMapContaCorrente");
     }
 }
